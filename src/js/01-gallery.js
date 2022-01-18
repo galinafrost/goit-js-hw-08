2e583a5cdf0a9609b1387cef18cac8965ddbeef3
galinafrost <morozgalina13@gmail.com>
2022-01-18 14:04:54 +0200

    1 task, 2 task, 3 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('.gallery');
+
+function createImageMarcup() {
+    const markup = galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    }).join('');
+
+    galleryContainerEl.insertAdjacentHTML('afterbegin', markup)
+};
+
+createImageMarcup();
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+    captionPosition: 'top',
+    navText: ['<', '>']
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+'use strict';
+import throttle from 'lodash.throttle';
+import localStorageApi from './localstorage';
 
+const contactFormEl = document.querySelector('.feedback-form');
+const formForData = {};
+
+contactFormEl.addEventListener('input', throttle((event) => {
+    const target = event.target;
+
+    formForData[target.name] = target.value;
+    localStorageApi.save('formForData', formForData);
+}, 500));
+
+contactFormEl.addEventListener('submit', event => {
+    event.preventDefault();
+
+    event.target.reset();
+    localStorageApi.remove('formForData');
+
+    console.log(formForData);
+});
+
+const fillFormFields = () => {
+    if (!localStorage.length) {
+      return;
+    }
+  
+    const localStorageFormData = localStorageApi.load('formForData');
+    const keys = Object.keys(localStorageFormData);
+  
+    for (const key of keys) {
+      contactFormEl.elements[key].value = localStorageFormData[key];
+    }
+  };
+  
+  fillFormFields();
